93c4c7f60181abd1f42683f83cfe5e5584d3255a
peter max <petermax2004@gmail.com>
2019-06-30 19:21:05 +0300

    Remove all files

--- app/auth/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
-from wtforms.validators import Required
-
-class CommentForm(FlaskForm):
-
-    title = StringField('Title',validators=[Required()])
-    comment = TextAreaField('Comment', validators=[Required()])
-    submit = SubmitField('Submit')
