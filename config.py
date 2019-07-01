f67331d26694f51355ac33e7a3a60ab6a05ad24b
peter max <petermax2004@gmail.com>
2019-07-01 10:12:25 +0300

    Add email configurations

--- config.py
+++ config.py
@@ -12,12 +12,12 @@ class Config:
     def init_app(app):
         pass
     
-    # #  email configurations
-    # MAIL_SERVER = 'smtp.googlemail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
 class ProdConfig(Config):
     '''
