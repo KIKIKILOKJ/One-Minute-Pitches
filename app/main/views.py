1a43d4f3629199704ffefa07276c1c9d00f3b619
peter max <petermax2004@gmail.com>
2019-07-01 11:21:51 +0300

    Add the profile route

--- app/main/views.py
+++ app/main/views.py
@@ -71,15 +71,15 @@ def new_pitch():
 
 
 
-# @main.route('/user/<uname>')
-# def profile(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     pitches = Pitch.query.filter_by(user_id = user.id)
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    pitches = Pitch.query.filter_by(user_id = user.id)
 
-#     if user is None:
-#         abort(404)
+    if user is None:
+        abort(404)
 
-#     return render_template("profile/profile.html", user = user,pitches=pitches)
+    return render_template("profile/profile.html", user = user,pitches=pitches)
 
 # @main.route('/user/<uname>/update',methods = ['GET','POST'])
 # @login_required
