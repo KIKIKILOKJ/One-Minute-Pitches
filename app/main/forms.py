a35e4f01d7768d9256cc14a497815d3ee6104694
peter max <petermax2004@gmail.com>
2019-07-01 11:25:14 +0300

    Add the PitchForm class

---
+++ app/main/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+class PitchForm(FlaskForm):
+    category = SelectField('Type',choices=[('Business','Business'), ('Software','Software')],validators=[Required()])
+    title = StringField('Pitch title',validators=[Required()])
+    pitch = TextAreaField('About Pitch')
+    submit = SubmitField('Submit')
+    
+# class UpdateProfile(FlaskForm):
+#     bio = TextAreaField('Tell us about you.',validators = [Required()])
+#     submit = SubmitField('Submit')
+    
+# class CommentsForm(FlaskForm):
+#     comment = TextAreaField('Add your Comments', validators=[Required()])
+#     submit = SubmitField('Submit Comment')
