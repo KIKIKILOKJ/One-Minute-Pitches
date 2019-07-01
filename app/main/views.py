18e6b2a266c89d53ad790905ddedc373b82d36dd
peter max <petermax2004@gmail.com>
2019-07-01 11:23:09 +0300

    Add pitches.html route

--- app/main/views.py
+++ app/main/views.py
@@ -100,25 +100,25 @@ def update_profile(uname):
 
     return render_template('profile/update.html',form =form)
 
-# @main.route('/user/<uname>/update/pic',methods= ['POST'])
-# @login_required
-# def update_pic(uname):
-#     user = User.query.filter_by(username = uname).first()
-#     if 'photo' in request.files:
-#         filename = photos.save(request.files['photo'])
-#         path = f'photos/{filename}'
-#         user.profile_pic_path = path
-#         db.session.commit()
-#     return redirect(url_for('main.profile',uname=uname))
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
 
 
-# @main.route('/pitch/<int:id>')
-# def single_pitch(id):
-#     pitch = Pitch.query.get(id)
-#     if pitch is None:
-#         abort(404)
-#     format_pitch = markdown2.markdown(pitch.pitch,extras=["code-friendly", "fenced-code-blocks"])
-#     return render_template('pitch.html',pitch = pitch,format_pitch=format_pitch)
+@main.route('/pitch/<int:id>')
+def single_pitch(id):
+    pitch = Pitch.query.get(id)
+    if pitch is None:
+        abort(404)
+    format_pitch = markdown2.markdown(pitch.pitch,extras=["code-friendly", "fenced-code-blocks"])
+    return render_template('pitch.html',pitch = pitch,format_pitch=format_pitch)
 
 
 # @main.route("/pitch/new/comment/<int:id>",methods=["GET","POST"])
