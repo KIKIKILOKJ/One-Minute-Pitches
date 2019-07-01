4ef67f744cea5f4827e0d7b85d94ce9d0cbc25d4
peter max <petermax2004@gmail.com>
2019-07-01 09:24:21 +0300

    Add decorator and class user

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from . import db
+from . import login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    pitch = db.relationship('Pitch',backref = 'user',lazy = "dynamic")
+    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+    
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
