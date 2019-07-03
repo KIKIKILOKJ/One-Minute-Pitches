6b5621df1560f09beb48809f448cf62bd63386e1
peter max <petermax2004@gmail.com>
2019-07-03 14:18:02 +0300

    recreate migrations

--- config.py
+++ config.py
@@ -5,12 +5,12 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://peter:abcdef@localhost/pitches'
+    
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL')
-
+    
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://peter:abcdef@localhost/pitches'
+    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://peter:abcdef@localhost/pitchess'
     DEBUG = True
 
 config_options = {
